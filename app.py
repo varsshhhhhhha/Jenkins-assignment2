db25bacaba4adc9e40b54f673f8043fe810858fe
Varsha <varsharam2003@gmail.com>
2025-02-15 17:32:13 +0530

    New change

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+print("New change to trigger Jenkins")
+
+@app.route('/')
+def home():
+    return "Hello, Jenkins CD with Flask and Docker!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
